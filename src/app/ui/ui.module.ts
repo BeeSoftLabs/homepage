7d23a3938d0e723b2b14b1f9ed24df3a3022e122
Derlys Dominguez <derlys275@gmail.com>
2020-11-09 19:58:12 -0500

    feat: create module ui and component navbar

---
+++ src/app/ui/ui.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+
+@NgModule({
+  declarations: [NavbarComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class UiModule { }
