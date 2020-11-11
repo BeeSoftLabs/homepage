a5f8d00c01ec428d80435cae699e96053f85451b
Angel <mario.velez@sofka.com.co>
2020-11-10 21:18:24 -0500

    feat: add @ngneat/tailwind

--- src/app/ui/header/header.component.ts
+++ src/app/ui/header/header.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-header',
   template: `
-    <div>
+    <div class="bg-purple-600">
       <ng-container *ngFor="let link of links">
         <a [routerLink]="link.path">{{ link.label }}</a> &nbsp;
       </ng-container>
