8b4e5ee5d0a64e39236ccec46134e3f77e860806
Derlys Dominguez <derlys275@gmail.com>
2020-11-12 18:51:00 -0500

    refactor: optimization of icons in lenguages

--- src/app/ui/lang-switcher/lang-switcher.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core';
-
-@Component({
-  selector: 'app-lang-switcher',
-  template: ``,
-})
-export class LangSwitcherComponent {
-  @Input() lang: 'en' | 'es' = 'es';
-}
--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -4,17 +4,11 @@ import { RouterModule } from '@angular/router';
 
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
-import { LangSwitcherComponent } from './lang-switcher/lang-switcher.component';
 import { LayoutComponent } from './layout/layout.component';
 import { UiService } from './ui.service';
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    LayoutComponent,
-    FooterComponent,
-    LangSwitcherComponent,
-  ],
+  declarations: [HeaderComponent, LayoutComponent, FooterComponent],
   imports: [CommonModule, RouterModule],
   providers: [UiService],
 })
